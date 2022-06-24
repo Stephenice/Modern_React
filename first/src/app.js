4960fda0cdfa8892105487c4ab09c50f0c5ffc60
stephenice <stephencode@hotmil.com>
2022-06-24 22:01:12 +0100

    react repo

---
+++ first/src/app.js
@@ -0,0 +1,11 @@
+import Navbar from "./navbar";
+
+const App = function () {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ first/src/index.js
@@ -0,0 +1,5 @@
+import react from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
