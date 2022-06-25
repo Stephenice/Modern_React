91c6e77c1cade029df15c7069bbeebe475e729e5
stephenice <stephencode@hotmail.com>
2022-06-25 19:33:06 +0100

    projects

--- first/src/app.js
+++ first/src/app.js
@@ -1,43 +1,39 @@
 import logo from "./logo192.png";
-import Style from "./index.css";
+import "./index.css";
 
 function Header() {
   return (
     <>
-      <header>
-        <nav>
-          <img src={logo} alt="reactimg" width={"40px"} />
-          <ul className="nav-ul">
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-        <p>React Course - Project 1</p>
-      </header>
+      <nav>
+        <img src={logo} alt="reactimg" width={"40px"} />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
+      </nav>
     </>
   );
 }
 
 function MainContent() {
   return (
-    <>
-      <h1> Fun facts about React</h1>
-      <ul>
+    <main>
+      <h1 className="main--title"> Fun facts about React</h1>
+      <ul className="main--facts">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-    </>
+    </main>
   );
 }
 
 function Footer() {
   return (
     <>
-      <footer>© 2022 Stephen development. All rights reserved.</footer>
+      <footer style={{ color: "white", backgroundColor: "gray" }}>
+        © 2022 Stephen development. All rights reserved.
+      </footer>
     </>
   );
 }
