80773ba7e6f212579fe1facc995d1a74aa0c4d35
stephenice <stephencode@hotmail.com>
2022-06-26 22:43:30 +0100

    Added project 3 season app

--- project3_weather/src/App.js
+++ project3_weather/src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-class App {}
+// class base component
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    console.log(React.Component);
+    return <>Latitude:</>;
+  }
+}
 
 export default App;
