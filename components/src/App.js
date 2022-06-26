26c9e5248f09633cabb9ad5f55b789312f24f588
stephenice <stephencode@hotmail.com>
2022-06-26 17:16:46 +0100

    Added faker dev and semantic-ui

--- components/src/App.js
+++ components/src/App.js
@@ -1,3 +1,25 @@
+import { faker } from "@faker-js/faker";
+
 export default function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <div className="ui comments container">
+        <h3 className="ui dividing header">Comments</h3>
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img alt="avatar" src={faker.image.avatar()} />
+          </a>
+          <div className="content">
+            <a href="/" className="author">
+              Matt
+            </a>
+            <div className="metadata">
+              <span className="date">Today at 5:42PM</span>
+            </div>
+            <div className="text">How artistic!</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
