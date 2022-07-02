a50faac341d4b5b4f6547ec41b7b4b3eff018440
stephenice <stephencode@hotmail.com>
2022-07-02 03:33:47 +0100

    monsters-rolodex

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * life cycle
+ * 1.constructor
+ * 2.render- will alway render anytime the state change
+ * 3. componentDidMount run once
+ *
+ */
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchValue: "",
+    };
+    // console.log("constructor");
+  }
+
+  // coomponentDidMount only invoke once
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log(this.state.searchValue);
+    return (
+      <div className="App">
+        <input
+          value={this.state.searchValue}
+          type="search"
+          className="search-box"
+          placeholder="search monsters"
+          onChange={(event) => {
+            this.setState({ searchValue: event.target.value });
+            // console.log(this.state.searchValue);
+          }}
+        />
+
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
